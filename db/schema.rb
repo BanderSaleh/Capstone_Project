164fb774117c9bb61a28a08b7628a082885abb9e
“BANDER <Bandersaleh93@gmail.com>
2020-08-22 19:24:01 -0700

    Added collumn image string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_012437) do
+ActiveRecord::Schema.define(version: 2020_08_23_004640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_012437) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "store_notes"
     t.string "timestamp"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
