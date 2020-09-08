e53d7ba9d4fed87871556a09465d175989ad149f
“BANDER <Bandersaleh93@gmail.com>
2020-09-08 01:02:27 -0700

    Basic Version 1.0 running

--- app/models/completed.rb
+++ app/models/completed.rb
@@ -1,3 +1,3 @@
 class Completed < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :products
-  has_many :completeds
+  # has_many :products
+  # has_many :completed
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
