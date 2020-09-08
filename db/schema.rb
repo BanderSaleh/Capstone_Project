08e88b85a3a116dd778f66c1fbcb8504f3be053d
“BANDER <Bandersaleh93@gmail.com>
2020-09-07 23:42:58 -0700

    Belongs to data should work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_054657) do
+ActiveRecord::Schema.define(version: 2020_09_08_064041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_054657) do
     t.string "store_notes"
     t.string "timestamp"
     t.string "picture"
-    t.string "user_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
