137f0ef9cd1da15db76ef11c9ea15ada9249bee8
“BANDER <Bandersaleh93@gmail.com>
2020-09-08 01:05:28 -0700

    Basic version 1.0 ready for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create!(email: "test@test.com", password: "password")
+user = User.create!(email: "test@test.com", password: "password")
 
-# Product.create!([
-#   {store_name: "Safeway", product_name: "Buttermilk Bread", quantity: 2, price: 5, deadline: "Wednesday", store_notes: "none", status: "Carted", store_notes_timestamp: "", timestamp: "", picture: "https://www.kroger.com/product/images/xlarge/front/0007313000125", user_id: user.id},
-#   {store_name: "Safeway", product_name: "Strawberries", quantity: 1, price: 5, deadline: "Wednesday", store_notes: "none", status: "Carted", timestamp: "", store_notes_timestamp: "", picture: "https://d2d8wwwkmhfcva.cloudfront.net/800x/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_48656b8f-d527-4e26-b24a-aa2bbba83b35.png", user_id: user.id},
-# ])
+Product.create!([
+  {store_name: "Safeway", product_name: "Buttermilk Bread", quantity: 2, price: 5, deadline: "Wednesday", store_notes: "none", status: "Carted", store_notes_timestamp: "", timestamp: "", picture: "https://www.kroger.com/product/images/xlarge/front/0007313000125", user_id: user.id},
+  {store_name: "Safeway", product_name: "Strawberries", quantity: 1, price: 5, deadline: "Wednesday", store_notes: "none", status: "Carted", timestamp: "", store_notes_timestamp: "", picture: "https://d2d8wwwkmhfcva.cloudfront.net/800x/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_48656b8f-d527-4e26-b24a-aa2bbba83b35.png"},
+])
 
 
